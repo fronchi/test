419071d8f3d5be9f8350ed3e43454ff0feb8030c
fronchi <34208704+fronchi@users.noreply.github.com>
2019-03-09 11:24:26 +0000

    commiting

---
+++ test.js
@@ -0,0 +1,14 @@
+var firebase = require("firebase");
+import firebase from "firebase";
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDpEF2QPYK4Fi09h7uQgfZ80RSwPsKqn5I",
+    authDomain: "testfelipe-f91f5.firebaseapp.com",
+    databaseURL: "https://testfelipe-f91f5.firebaseio.com",
+    projectId: "testfelipe-f91f5",
+    storageBucket: "testfelipe-f91f5.appspot.com",
+    messagingSenderId: "656014133333"
+  };
+  firebase.initializeApp(config);
+  </script>
